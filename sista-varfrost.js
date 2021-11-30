a57acdd49689a8bdc91a8dadd2f494bc5900b146
Jeremy Karlsson <karlsson@jeremy.se>
2021-11-30 16:58:38 +0100

    Update parsing of layer

--- sista-varfrost.js
+++ sista-varfrost.js
@@ -58,9 +58,51 @@ async function handle(request) {
     const { lng, lat } = validateSearchQuery(url);
 
     const data = await get([lng, lat]);
-    const feature = data.features[0];
+    const symbol = data.features[0].properties['SYMBOL'];
 
-    return new Response(JSON.stringify({ data, feature }, null, prettyPrint(request) ? 4 : undefined), {
+    let range;
+
+    switch (symbol) {
+        case 1:
+            range = {
+                from: '--04-01',
+                to: '--04-15'
+            };
+            break;
+        case 2:
+            range = {
+                from: '--04-15',
+                to: '--05-01'
+            };
+            break;
+        case 3:
+            range = {
+                from: '--05-01',
+                to: '--05-15'
+            };
+            break;
+        case 4:
+            range = {
+                from: '--05-15',
+                to: '--06-01'
+            };
+            break;
+        case 5:
+            range = {
+                from: '--06-01',
+                to: '--06-15'
+            };
+            break;
+        case 6:
+        default:
+            range = {
+                from: '--06-15',
+                to: null
+            };
+            break;
+    }
+
+    return new Response(JSON.stringify({ range }, null, prettyPrint(request) ? 4 : undefined), {
         status: 200,
         headers: new Headers({
             'content-type': 'application/json'
