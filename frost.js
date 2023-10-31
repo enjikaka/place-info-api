c1af1ac61c6c98de0f8e19965f193ec8cfeea464
Jeremy Karlsson <karlsson@jeremy.se>
2023-10-31 18:34:38 +0100

    Refactor frost test

--- frost.js
+++ frost.js
@@ -1,48 +1,40 @@
 import { cachedResponse, getMetaData, validateSearchQuery, getData, findValue, shortMonthToNum } from './helpers.js';
 
-async function getHostfrost(request) {
-  function fixValue(value) {
-    const [day, monthString] = value.split(' ');
-
-    const monthNumber = shortMonthToNum(monthString);
-
-    return `--${monthNumber}-${day}`;
-  }
-
-  const url = new URL(request.url);
-  const { lng, lat } = validateSearchQuery(url);
-
-  const data = await getData([lng, lat], {
+/**
+ *
+ * @param {Coordinates} coords
+ * @returns
+ */
+export async function getFirstAutumnFrostDate(coords) {
+  const data = await getData(coords, {
     wms: 'https://opendata-view.smhi.se/klim-stat_is/forsta_hostfrost/wms',
     layers: ['klim-stat_is:forsta_hostfrost_yta']
   });
 
-  const rawValue = findValue(data);
+  const rawValue = findValue(data); // Example: "1 okt"
+  const [day, monthString] = rawValue.split(' ');
+  const monthNumber = shortMonthToNum(monthString);
 
-  return fixValue(rawValue);
+  return `--${monthNumber}-${day}`;
 }
 
-async function getVarfrost(request) {
-  function fixValue(value) {
-    const [fromDay, toDay] = value.split('-').map(n => parseInt(n, 10));
-
-    const [, monthString] = value.split(' ');
-    const monthNumber = shortMonthToNum(monthString);
-
-    return `--${monthNumber}-${fromDay}/--${monthNumber}-${toDay}`;
-  }
-
-  const url = new URL(request.url);
-  const { lng, lat } = validateSearchQuery(url);
-
-  const data = await getData([lng, lat], {
+/**
+ *
+ * @param {Coordinates} coords
+ * @returns
+ */
+export async function getLastSpringFrostDate(coords) {
+  const data = await getData(coords, {
     wms: 'https://opendata-view.smhi.se/klim-stat_is/sista_varfrost/wms',
     layers: ['klim-stat_is:sista_varfrost_yta']
   });
 
-  const rawValue = findValue(data);
+  const rawValue = findValue(data); // Example: "1-15 maj"
+  const [days, monthString] = rawValue.split(' ');
+  const [fromDay, toDay] = days.split('-').map(n => parseInt(n, 10));
+  const monthNumber = shortMonthToNum(monthString);
 
-  return fixValue(rawValue);
+  return `--${monthNumber}-${fromDay}/--${monthNumber}-${toDay}`;
 }
 
 /**
@@ -50,16 +42,20 @@ async function getVarfrost(request) {
  * @returns {Promise<Response>}
  */
 export async function handler(request) {
-  const [hostfrost, varfrost, metadata] = await Promise.all([
-    getHostfrost(request),
-    getVarfrost(request),
+  const url = new URL(request.url);
+  const { lng, lat } = validateSearchQuery(url);
+  const coords = [lng, lat];
+
+  const [höstfrost, vårfrost, metadata] = await Promise.all([
+    getFirstAutumnFrostDate(coords),
+    getLastSpringFrostDate(coords),
     getMetaData('https://opendata-view.smhi.se/klim-stat_is/sista_varfrost/wms')
   ]);
 
   return cachedResponse({
     value: {
-      hostfrost,
-      varfrost
+      höstfrost,
+      vårfrost
     },
     metadata
   }, request);
--- frost_test.ts
+++ frost_test.ts
@@ -1,6 +1,20 @@
 import { assertEquals } from "https://deno.land/std@0.204.0/assert/mod.ts";
-import { handler } from './frost.js';
+import { handler, getFirstAutumnFrostDate, getLastSpringFrostDate } from './frost.js';
+import type { Coordinates } from './helpers.js';
 
+const barlingshultCoords: Coordinates = [12.312287, 59.512382];
+
+Deno.test('getFirstAutumnFrostDate', async () => {
+  const autumnFrostDate = await getFirstAutumnFrostDate(barlingshultCoords);
+
+  assertEquals(autumnFrostDate, '--10-1');
+});
+
+Deno.test('getLastSpringFrostDate', async () => {
+  const springFrostDate = await getLastSpringFrostDate(barlingshultCoords);
+
+  assertEquals(springFrostDate, '--05-1/--05-15');
+});
 
 Deno.test('handler', async () => {
   const url = new URL('/?lat=59.512382&lng=12.312287', 'http://localhost:8080');
@@ -11,8 +25,8 @@ Deno.test('handler', async () => {
 
   const expected = {
     value: {
-      hostfrost: "--10-1",
-      varfrost: "--05-1/--05-15"
+      höstfrost: "--10-1",
+      vårfrost: "--05-1/--05-15"
     },
     metadata: {
       description: "<redacted>",
